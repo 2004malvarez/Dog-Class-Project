07d5f09a27581d1ed1b15039d524a9fc7ca49920
marcos alvarez <2004malvarez@gmail.com>
2026-01-22 14:06:36 -0600

    public class section

--- DogClassProgram/DogClassProgram.cpp
+++ DogClassProgram/DogClassProgram.cpp
@@ -10,3 +10,34 @@ private:
     string breed;
 
 public:
+    // Setter functions
+    void setName(string n) {
+        name = n;
+    }
+
+    void setAge(int a) {
+        age = a;
+    }
+
+    void setBreed(string b) {
+        breed = b;
+    }
+
+    // Getter functions
+    string getName() {
+        return name;
+    }
+
+    int getAge() {
+        return age;
+    }
+
+    string getBreed() {
+        return breed;
+    }
+
+    // Bark function
+    void bark() {
+        cout << "Woof! Woof!" << endl;
+    }
+};
