dbd97f100fe65ba012498098aa464faa5245bd00
marcos alvarez <2004malvarez@gmail.com>
2026-01-22 14:10:20 -0600

    Main function

--- DogClassProgram/DogClassProgram.cpp
+++ DogClassProgram/DogClassProgram.cpp
@@ -41,3 +41,29 @@ public:
         cout << "Woof! Woof!" << endl;
     }
 };
+
+int main() {
+    // Create a Dog object
+    Dog myDog;
+
+    // Set initial values
+    myDog.setName("Buddy");
+    myDog.setAge(3);
+    myDog.setBreed("Golden Retriever");
+
+    // Display dog information using getters
+    cout << "Dog Name: " << myDog.getName() << endl;
+    cout << "Dog Age: " << myDog.getAge() << endl;
+    cout << "Dog Breed: " << myDog.getBreed() << endl << endl;
+
+    // Modify one attribute using a setter
+    myDog.setAge(4);
+
+    cout << "After updating age:" << endl;
+    cout << "Dog Age: " << myDog.getAge() << endl;
+
+    // Call the bark function
+    myDog.bark();
+
+    return 0;
+}
